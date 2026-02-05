7af05de9cf8d21f3ad8c9917999355cb8bd6750b
Hajare Oulbaz <hajare.oulbaz@etu.univ-nantes.fr>
2026-02-05 22:01:16 +0100

    connect QuickSave button to saveAs action

--- leafwriter/packages/cwrc-leafwriter/src/components/bottombar/quickSave/index.tsx
+++ leafwriter/packages/cwrc-leafwriter/src/components/bottombar/quickSave/index.tsx
@@ -3,25 +3,25 @@
 import { Box } from '@mui/material';
 import { ActionButtons } from './ActionButtons';
 
+import { useActions } from '../../../overmind';
+
 const QuickSave = () => {
-  // 1. Logic layer: 
-  // Future implementation: const { saveDocument } = useQuickSave(); 
+  const { triggerSaveAs } = useActions().editor;
 
   const handleSave = () => {
-    console.log('Save Clicked');
-    alert('Save function triggered!');
+    triggerSaveAs();
   };
 
   return (
     // 2. Layout layer key point:
     // ml: 'auto' (Margin Left Auto) pushes this container to the far right 
     // of the Flexbox parent by consuming all available space.
-    <Box 
-      sx={{ 
-        display: 'flex', 
-        alignItems: 'center', 
-        ml: 'auto', 
-        mr: 2 
+    <Box
+      sx={{
+        display: 'flex',
+        alignItems: 'center',
+        ml: 'auto',
+        mr: 2
       }}
     >
       <ActionButtons onSave={handleSave} />
