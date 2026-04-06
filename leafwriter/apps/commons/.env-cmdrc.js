94245a3a99b6887f3abf72e8a9d774d41f99efd9
makabaka <jingxuan@qq.com>
2026-04-06 12:58:25 +0200

    feat: support simultaneous execution of Publish and local versions

--- leafwriter/apps/commons/.env-cmdrc.js
+++ leafwriter/apps/commons/.env-cmdrc.js
@@ -1,16 +1,25 @@
 const envs = {
   GA_MEASUREMENT_ID: 'G-JG3NWYH6TY',
   GEONAMES_USERNAME: 'LEAFwriter',
-  KEYCLOAK_URL: 'https://keycloak.dev.lincsproject.ca',
+  // KEYCLOAK_URL: 'https://keycloak.dev.lincsproject.ca', // 已迁移到 GitHub OAuth，不再使用
   AUTH_API_URL: 'https://auth-api.dev.lincsproject.ca',
 };
 
 module.exports = {
+  // ========== LOCAL DEV ==========
   development: {
     ...envs,
+    AUTH_API_URL: 'http://localhost:8091',
     NODE_ENV: 'development',
     WORKER_ENV: 'production',
   },
+  // ========== PUBLISH TO VM ==========
+  // development: {
+  //   ...envs,
+  //   AUTH_API_URL: 'https://auth-api.dev.lincsproject.ca',
+  //   NODE_ENV: 'development',
+  //   WORKER_ENV: 'production',
+  // },
   ...envs,
   'development-worker-dev': {
     NODE_ENV: 'development',
