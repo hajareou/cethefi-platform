32ee100ebc6cac88d7743559046efab0e4a82650
makabaka <jingxuan@qq.com>
2026-05-01 17:25:58 +0200

    delate:dataLoader

--- src/services/dataLoader.js
+++
@@ -1,7 +0,0 @@
-export async function loadJson(path) {
-  const res = await fetch(path)
-  if (!res.ok) {
-    throw new Error(`Failed to load ${path} (HTTP ${res.status})`)
-  }
-  return res.json()
-}
